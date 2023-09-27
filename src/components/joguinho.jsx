295c29cc0564aafaa601aa1827c3aefab6e381f4
Samara Luiza Cunha Silva <106708531+samaralcunha@users.noreply.github.com>
2023-09-27 11:51:09 -0300

    joguinho parte 1

--- src/components/joguinho.jsx
+++ src/components/joguinho.jsx
@@ -1,5 +1,46 @@
 import React, { useState } from 'react';
 
 export default Joguinho({ placar, setPlacar }) {
+    const [jogador, setJogador] = useState(null);
+    const [computador, setComputador] = useState(null);
+    const opcoes = ['Pedra', 'Papel', 'Tesoura'];
+    const [resultado, setResultado] = useState('');
+
+    const escolhaComputador = () => {
+        const escolha = opcoes[Math.floor(Math.random() * opcoes.length)];
+        serComputador(escolha);
+        return escolha;
+    };
+
+    const verificaResultado = (jogador, computador) => {
+        if( (jogador === 'Pedra' && computador === 'Tesoura') || (jogador === 'Papel' && computador === 'Pedra') || (jogador === 'Tesoura' && computador ==='Papel') ) {
+            setPlacar((prevPlacar) => ({...prevPlacar, jogador: prevPlacar.jogador + 1}));
+            setResultado("Ganhou hulhul ^^"); 
+        } else if (jogador === computador) {
+            setPlacar((prevPlacar) => ({...prevPlacar, empate: prevPlacar.empate + 1}));
+            setResultado('Empatouuuuu :/');
+        } else {
+            setPlacar((prevPlacar) => ({...prevPlacar, computador: prevPlacar.computador + 1}));
+            setResultado('Perdeu pra um robo haha ><');
+        }
+    };
+
+    const jogar = (escolha) => {
+        setJogador(escolha);
+        const escolherComputador = escolhaComputador();
+        verificaResultado(escolha, escolherComputador);
+    };
+
+    const reiniciarJogo = () =>{
+        setPlacar({jogador: 0, computador: 0, empate: 0});
+        setJogador(null);
+        setComputador(null);
+        setResultado('');
+    };
+
+    return (
+        
+    )
+
 
 }
